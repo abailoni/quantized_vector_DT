633edf15ec719254b724e3e9a54265104968458b
c-laun <christian.laun@gmail.com>
2019-03-06 14:01:43 +0100

    Now also prints if cuda is available

--- first_test.py
+++ first_test.py
@@ -1,8 +1,11 @@
 import numpy as np
 import torch
-
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 print('hello world')
 
 print(np.random.rand())
+
+print(f'Does this device have CUDA? \n The answer is: {torch.cuda.is_available()}')
