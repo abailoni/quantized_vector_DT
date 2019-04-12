f1ca1f5d58d501832b2f18a2f6d7cd0228aafb50
c-laun <christian.laun@gmail.com>
2019-04-12 15:25:24 +0200

    added comments and some work in progress

--- quantizedVDT/transforms.py
+++ quantizedVDT/transforms.py
@@ -1,26 +1,31 @@
 import numpy as np
 
 from inferno.io.transform import Transform
-
+from quantizedVDT.utils.affinitiy_utils import get_offset_locations
 from stardist import star_dist
 
 
 class DirectionsToAffinities(Transform):  # not functional atm, do not use
 
     def __init__(self,  n_directions=8, z_direction=False):
+        raise NotImplementedError('remove this error if function is functional')
         super().__init__()
         self.n_directions = n_directions
-        self.default_offsets = [1, 3, 9, 27]
-        self.default_z_offsets = [1, 2, 3, 4]
+        self.default_distances = [1, 3, 9, 27]
+        self.default_z_distances = [1, 2, 3, 4]
         self.z_direction = z_direction
+        self.offsets = []
+        if self.z_direction:
+            self.offsets += [[-1, 0, 0], [-2, 0, 0], [-3, 0, 0], [-4, 0, 0]]
+            self.offsets += [[1, 0, 0], [2, 0, 0], [3, 0, 0], [4, 0, 0]]
+        for i in range(self.n_directions):
+            angle = 2*np.pi/self.n_directions*i
+            self.offsets += get_offset_locations(self.default_distances, angle)
 
     def batch_function(self, tensors):
         prediction, target = tensors
 
-        if self.z_direction:
-            affinities = np.empty(())
-            for i in range(2):
-                pass
+
 
 
 
@@ -31,13 +36,6 @@ class LabelToDirections(Transform):
         self.opencl_available = opencl_available
         self.compute_z = compute_z
 
-
-    '''
-    def volume_function(self, tensor):
-        output = sdist_volume(tensor, self.n_directions,
-                              opencl_available=self.opencl_available)
-        return output
-    '''
 
     def batch_function(self, tensors):
         prediction, target = tensors
@@ -51,12 +49,7 @@ class LabelToDirections(Transform):
             distances = sdist_volume(target, self.n_directions,
                                      opencl_available=self.opencl_available)
         return prediction, distances
-    """
-    def batch_function(self, tensors):
-        prediction, target = tensors
-        target = np.moveaxis(star_dist(target[0].numpy(), self.n_directions, opencl=self.opencl_available), -1, 0)
-        return prediction, target
-    """
+
 
 
 def sdist_volume(vol, n_directions, opencl_available=True):
@@ -77,7 +70,11 @@ def sdist_volume(vol, n_directions, opencl_available=True):
 
 
 def z_dist(vol):
-
+    """
+    Compute the distance to the next change in label in positive and negative z-direction
+    :param vol: Volume of image labels
+    :return: 4d-Array with shape (2, *vol) that holds the distances
+    """
     distances = np.zeros((2, *vol.shape), dtype=np.float32)
     zeros_2d = np.zeros(vol.shape[1:], dtype=np.float32)
     for z in range(1, vol.shape[0]):
@@ -88,6 +85,12 @@ def z_dist(vol):
 
 
 def distancetoaffinities(distance, offsets): #Work in Progress
+    raise NotImplementedError
     # return 1.0 if the distance is smaller than the offset, 0.0 if not.
     return [float(distance <= offset) for offset in offsets]
+
+
+
+
+print('halt, stop!')
 
