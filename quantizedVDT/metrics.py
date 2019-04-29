62f86f32f0baf281a0cc7ed49c252ec3c2e9edbe
c-laun <christian.laun@gmail.com>
2019-04-29 15:59:21 +0200

    added support for multiple batches

--- quantizedVDT/metrics.py
+++ quantizedVDT/metrics.py
@@ -1,6 +1,7 @@
 from neurofire.metrics.arand import ArandErrorFromMWS
 import numpy as np
 from quantizedVDT.utils.affinitiy_utils import get_offset_locations
+from speedrun.log_anywhere import log_embedding, log_image
 
 
 class ArandFromMWSDistances(ArandErrorFromMWS):
@@ -9,6 +10,7 @@ class ArandFromMWSDistances(ArandErrorFromMWS):
                  strides=None, randomize_strides=False,
                  **super_kwargs):
 
+        self.laststep = None
         self.n_directions = n_directions
         self.default_distances = [1, 3, 9, 27]
         self.default_z_distances = [1, 2, 3, 4]
@@ -23,24 +25,32 @@ class ArandFromMWSDistances(ArandErrorFromMWS):
         super().__init__(self.offsets, strides=strides, randomize_strides=randomize_strides,
                          **super_kwargs)
 
-    def input_to_segmentation(self, distances):  # pray before testing
+    def input_to_segmentation(self, distances):
+        # FIXME: Works for one batch only atm
         # input: Distances in N directions
         # TODO: turn distances into affinities
-        # hardcode that we have 4 affinities per direction
-        affinities = np.empty((4*distances.shape[0], *distances.shape[1:]))
-        k = 0
-        if self.z_direction:
-            for i, z_distance in enumerate(self.default_z_distances):
-                affinities[i+k*4, :, :, :] = np.where(distances[k] <= z_distance, 0, 1)
-            k += 1
-            for i, z_distance in enumerate(self.default_z_distances):
-                affinities[i + k * 4, :, :, :] = np.where(distances[k] <= z_distance, 0, 1)
-            k += 1
+        # hardcoded that we have 4 affinities per direction
+        affinities = np.empty((distances.shape[0], 4*distances.shape[1], *distances.shape[2:]))
+        for batchnum in range(distances.shape[0]):
+            k = 0
+            if self.z_direction:
+                for i, z_distance in enumerate(self.default_z_distances):
+                    affinities[batchnum, i+k*4, :, :, :] = np.where(distances[batchnum, k] <= z_distance, 1, -1)
+                k += 1
+                for i, z_distance in enumerate(self.default_z_distances):
+                    affinities[batchnum, i+k*4, :, :, :] = np.where(distances[batchnum, k] <= z_distance, 1, -1)
+                k += 1
 
-        while k < distances.shape[0]:
-            for i, xy_distance in enumerate(self.default_distances):
-                affinities[i+k*4] = np.where(distances[k] <= xy_distance, 0, 1)
-            k += 1
+            while k < distances.shape[1]:
+                for i, xy_distance in enumerate(self.default_distances):
+                    affinities[batchnum, i+k*4] = np.where(distances[batchnum, k] <= xy_distance, 1, -1)
+                k += 1
 
-#        affinities =
+
+            #log_embedding('tensor', affinities[0, 0, 1])
+            #for i in range(affinities.shape[1]):
+            #    log_image(f'affinities/channel{i}', affinities[0, i, 1])
+                # not only not pretty but also depends on a modification I made to the logger
+                # addition was made in speedrun/tensorboard.py lines 51 and 55
+    #        affinities =
         return super(ArandFromMWSDistances, self).input_to_segmentation(affinities)
