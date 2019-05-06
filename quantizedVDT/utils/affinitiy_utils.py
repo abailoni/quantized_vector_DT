fixed sign error

quantizedVDT/utils/affinitiy_utils.py
@@ -10,7 +10,7 @@ def get_offset_locations(offset_distances, angle):
     """
     loc = []
     for dist in offset_distances:
-        loc += [[0, int(round(-1*dist*np.sin(angle))), int(round(dist*np.cos(angle)))]]
+        loc += [[0, int(round(1*dist*np.sin(angle))), int(round(dist*np.cos(angle)))]]
     return loc
 
 