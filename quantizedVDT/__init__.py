1af4274540f7060abfb2c467a445fad386577a5f
c-laun <christian.laun@gmail.com>
2019-07-26 15:13:53 +0200

    Not needed atm

--- quantizedVDT/__init__.py
+++ quantizedVDT/__init__.py
@@ -6,19 +6,20 @@ matplotlib.use('Agg')
 from .utils.path_utils import get_abailoni_hci_home_path
 import sys
 import os
-
-sys.path += [
-os.path.join(get_abailoni_hci_home_path(), "python_libraries/nifty/python"),
-os.path.join(get_abailoni_hci_home_path(), "python_libraries/cremi_python"),
-os.path.join(get_abailoni_hci_home_path(), "python_libraries/affogato/python"),
-os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/inferno"),
-os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/constrained_mst"),
-os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/neuro-skunkworks"),
-os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/segmfriends"),
-os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/speedrun"),
-os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/embeddingutils"),
-os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/firelight"),
-# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/hc_segmentation"),
-os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/neurofire"),]
+#
+# sys.path += [
+# os.path.join(get_abailoni_hci_home_path(), "python_libraries/nifty/python"),
+# os.path.join(get_abailoni_hci_home_path(), "python_libraries/cremi_python"),
+# # os.path.join(get_abailoni_hci_home_path(), "python_libraries/affogato/python"),
+# "/export/home/claun/PycharmProjects/affogato/python",
+# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/inferno"),
+# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/constrained_mst"),
+# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/neuro-skunkworks"),
+# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/segmfriends"),
+# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/speedrun"),
+# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/embeddingutils"),
+# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/firelight"),
+# # os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/hc_segmentation"),
+# os.path.join(get_abailoni_hci_home_path(), "pyCharm_projects/neurofire"),]
 
 
