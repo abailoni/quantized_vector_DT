7ed74b330a8831aba0521df4b27f8c83c298f0ec
c-laun <christian.laun@gmail.com>
2019-07-26 15:06:45 +0200

    Applies Softmax only to some values

---
+++ quantizedVDT/utils/activations.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch 
+import torch.nn as nn
+
+
+class PartialSoftmax(nn.Softmax):
+
+    def __init__(self, dim=None, nclasses=4, ndirs=8):
+
+        if dim is None:
+            dim = -4
+        super(PartialSoftmax, self).__init__(dim=dim)
+
+        self.nclasses = nclasses
+        self.ndirs = ndirs
+
+
+    def forward(self, input):
+        shape = input.shape
+
+        classes = input[:8*4].reshape(shape[0], self.ndirs, self.nclasses, *shape[2:])
+
+        super(PartialSoftmax, self).forward(classes)
+
+        input[:, self.ndirs*self.nclasses] = classes.reshape(shape[0], self.ndirs*self.nclasses, *shape[2:])
+
+        return input
+
